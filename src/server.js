518ae20ad5d5239049b4ff0163324b9174948b40
Omkar Patki <omkarpatki07@gmail.com>
2026-02-18 15:39:03 +0530

    chnages in server.js file and .env file with correct api

--- src/server.js
+++ src/server.js
@@ -17,7 +17,12 @@ const io = socketIo(server, {
   cors: { origin: process.env.CLIENT_URL }
 });
 
-app.use(cors());
+app.use(
+  cors({
+    origin: process.env.CLIENT_URL,
+    credentials: true,
+  })
+);
 app.use(express.json());
 app.use(rateLimiter);
 
